c92c7b1a1958ab68f948cc01be67d68431458a56
Lizette Vazquez <lizette.vazquez6156@alumnos.udg.mx>
2025-04-04 22:39:57 -0600

    mejora vercellll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import { useEffect, useState } from "react";
-import { obtenerUsuarios } from "./api.js";
+import React, { useEffect, useState } from "react";
+import { fetchUsuarios } from "./api";
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
-    obtenerUsuarios().then(setUsuarios);
+    fetchUsuarios().then(setUsuarios);
   }, []);
 
   return (
     <div>
       <h1>Lista de Usuarios</h1>
-      <ul>
-        {usuarios.map((usuario) => (
-          <li key={usuario.id}>{usuario.nombre}</li>
-        ))}
-      </ul>
+      {usuarios.length > 0 ? (
+        <ul>
+          {usuarios.map((usuario) => (
+            <li key={usuario.id}>
+              {usuario.nombre} - {usuario.email}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No hay usuarios o no se pudo conectar.</p>
+      )}
     </div>
   );
 }
 
 export default App;
-
